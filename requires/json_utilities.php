45730dd79660269c6550ab68ef20563f2e840ce1
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2025-03-10 11:40:44 +0100

    modification de l'utilisateur ajoutée en php

--- requires/json_utilities.php
+++ requires/json_utilities.php
@@ -1,5 +1,5 @@
 <?php
-	function addJSONContent($file, $array){
+	function ajouterContenuJson($file, $array){
 		$file_content = file_get_contents($file);
 		$content = json_decode($file_content, true); 
 		array_push($content, $array);
@@ -7,11 +7,109 @@
 		return 0;
 	}
 
-	function readJSONContent($file){
+	function lireFichierJson($file){
 		$file_content = file_get_contents($file);
 		$content = json_decode($file_content, true); 
 		return $content;
 	}
+
+	function trouverUtilisateurAvecId($user_tab, $id){ # Retourne la ligne avec l'id correspondant dans le tableau (0 si non trouvé). Ca commence à 1
+		foreach ($user_tab as $key => $value) {
+			if($value["id"] == $id){
+				return $key+1;
+			}
+		}
+		return 0;
+	}
+
+	function modifierNomUtilisateur($id, $nouveauNom){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["nom"] = $newName;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+	function modifierPrenomUtilisateur($id, $nouveauPrenom){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["prenom"] = $newName;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	function modifierCourrielUtilisateur($id, $nouveauCourriel){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["courriel"] = $newMail;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+	
+	function modifierRoleUtilisateur($id, $nouveauRole){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["role"] = $newRole;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	function modifierMotDePasseUtilisateur($id, $nouveauMdp){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["mdp"] = $newMdp;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	function modifierNaissanceUtilisateur($id, $nouvelleNaissance){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["naissance"] = $newNaissance;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	function modifierGenreUtilisateur($id, $nouveauGenre){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["genre"] = $newGenre;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	function modifierTelUtilisateur($id, $nouveauTel){
+		$file_content = file_get_contents($file);
+		$content = json_decode($file_content, true);
+		$line = findUserWithId($content, $id);
+		if($line != 0){
+			$content[$line-1]["tel"] = $newTel;
+			file_put_contents($file,json_encode($content));
+			return 0;
+		}
+	}
+
+	
 
 	//EXP UTILISATION
 
