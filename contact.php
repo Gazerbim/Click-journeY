9be85f255e8579436cc349bc62f781061a95210a
tom200504 <166423945+tom200504@users.noreply.github.com>
2025-03-16 12:31:57 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -27,7 +27,7 @@
                 <a href="connexion.php"><button>Connexion</button></a>
                 <a href="profil.php"><button>
 		<?php
-        		session_start(); 
+        		
         		if (isset($_SESSION['prenom']) && !empty($_SESSION['prenom'])) {
             			echo $_SESSION['prenom'];
         		} 
