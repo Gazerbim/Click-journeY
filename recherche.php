152277d709d4190199942ff670ce5fe83df90831
tom200504 <166423945+tom200504@users.noreply.github.com>
2025-04-29 18:55:27 +0200

    js

--- recherche.php
+++ recherche.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rush&Krous - Recherche</title>
 </head>
-<body class="rech">
+<body class="rech light-mode">
  <?php
     session_start();
     include('requires/header.php');
@@ -53,5 +53,6 @@
     <?php
         require('requires/footer.php');
     ?>
+    <script src="script.js"></script>
 </body>
 </html>
